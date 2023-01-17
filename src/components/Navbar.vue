f7a324d9eebcf8973412d8b2b738815b40484ed8
cwiklik.wojciech <cwiklik.wojciech@gmail.com>
2023-01-17 23:17:42 +0100

    [development] - Navbar, routing

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <h1>Personal Blog</h1>
+        <nav>
+            <router-link :to="{ name: 'home' }">Home</router-link> 
+            <router-link :to="{ name: 'createPost' }">Create Post</router-link>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    header{
+        display: flex;
+        justify-content: space-between;
+        max-width: 1200px;
+        align-items: center;
+        margin: 0 auto;
+        padding: 10px;
+    }
+    header h1{
+        color: hsl(0deg 2% 62% / 72%);
+        font-size: 48px;
+    }
+    header a{
+       text-decoration: none;
+       margin-left: 20px;
+    }
+    header a.router-link-active{
+        color: #42b983;
+        font-weight: bold;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-   
+    <h1>Home</h1>
   </div>
 </template>
 
@@ -8,6 +8,16 @@
 
 
 export default {
-  name: 'HomeView'
+ 
 }
 </script>
+
+<style>
+  .home{
+    max-width: 1200px;
+    margin: 0 auto;
+    align-items: center;
+    justify-content: space-between;
+    display: flex;
+  }
+</style>
